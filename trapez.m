093a42728a36ea5c46709f11feadd73a8b1a4fb5
Sc1pex <scipex.dev@gmail.com>
2025-06-02 12:04:55 +0300

    fix trapez.m

--- trapez.m
+++ trapez.m
@@ -1,5 +1,5 @@
 function [T] = trapez(f, a, b, n)
     h = (b - a) / n;
     x = a + h : h : b - h;
-    T = h * feval(f, a) + feval(f, b) + sum(feval(f, x));
+    T = h * (feval(f, a) / 2 + feval(f, b) / 2 + sum(feval(f, x)));
 end
